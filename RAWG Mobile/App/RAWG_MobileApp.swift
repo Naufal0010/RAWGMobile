b148c89e7595f2c8c2d35ecf960e943d8a85f2f2
Naufal Abiyyu <naufal1068@gmail.com>
2024-03-01 11:36:14 +0800

    Initial Commit

---
+++ RAWG Mobile/App/RAWG_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAWG_MobileApp.swift
+//  RAWG Mobile
+//
+//  Created by Naufal Abiyyu on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct RAWG_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
